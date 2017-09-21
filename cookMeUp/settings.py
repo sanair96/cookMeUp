b865d9f2a99bb8fd83565c547616588ce50f4d65
Ranjitha96 <ranjitha.ramesh10@gmail.com>
2017-09-21 12:51:16 +0530

    ingredients intial

--- cookMeUp/settings.py
+++ cookMeUp/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'cookMeUp',
         'USER' : 'postgres',
-        'PASSWORD' : 'sandy123',
+        'PASSWORD' : 'thara96',
         'HOST' :'',
         'PORT' : '',
     },
