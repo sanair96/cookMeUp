d5f7fd7151ae739fe737abad381f94344afea001
shubh1996 <shubhmehta1996@gmail.com>
2017-09-21 17:59:55 +0530

    ranjithaa

--- ingredients/views.py
+++ ingredients/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 def home(request):
     if(request.method == 'GET'):
-        return render(request, 'base.html')
+        return render(request, 'dishes.html')
