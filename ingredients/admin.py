6ccc7cc22e4b6b0efa8c460c64efebea977d046f
Ranjitha96 <ranjitha.ramesh10@gmail.com>
2017-10-27 17:34:23 +0530

    Added a lot of functionality

--- ingredients/admin.py
+++ ingredients/admin.py
@@ -4,20 +4,20 @@ from django.contrib import admin
 
 from .models import dish, com_ingredients, maj_ingredients
 
-class dishAdmin(admin.ModelAdmin):
-    class Meta:
-        model = dish
+# class dishAdmin(admin.ModelAdmin):
+#     class Meta:
+#         model = dish
 
-class majAdmin(admin.ModelAdmin):
-    class Meta:
-        model = maj_ingredients
+# class majAdmin(admin.ModelAdmin):
+#     class Meta:
+#         model = maj_ingredients
 
-class comAdmin(admin.ModelAdmin):
-    class Meta:
-        model = com_ingredients
+# class comAdmin(admin.ModelAdmin):
+#     class Meta:
+#         model = com_ingredients
 
 
 
-admin.site.register(dish,dishAdmin)
-admin.site.register(maj_ingredients,majAdmin)
-admin.site.register(com_ingredients,comAdmin)
+# admin.site.register(dish,dishAdmin)
+# admin.site.register(maj_ingredients,majAdmin)
+# admin.site.register(com_ingredients,comAdmin)
--- ingredients/views.py
+++ ingredients/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
-
+import psycopg2
 # Create your views here.
 
 def home(request):
-    if(request.method == 'GET'):
-        return render(request, 'dishes.html')
+	if(request.method == 'GET'):
+		conn = psycopg2.connect("host='localhost' dbname='cookMeUp' user='postgres' password='thara96'")
+		cursor = conn.cursor()
+		cursor.execute('select * from min_ingredient');
+		records = cursor.fetchall()
+		cursor.execute('select * from major_ingredient');
+		recor = cursor.fetchall()
+		cursor.close()
+		return render(request, 'ingredients.html',{'maj':recor,'min':records})
+
+def req(request):
+	if(request.method == 'GET'):
+		return render(request, 'ingredients.html')
+	data = request.data
+	fop = data['fop']
+	sop = data['sop']
+	conn = psycopg2.connect("host='localhost' dbname='cookMeUp' user='postgres' password='thara96'")
